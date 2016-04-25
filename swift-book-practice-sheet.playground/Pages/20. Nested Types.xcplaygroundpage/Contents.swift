d8aca06c4aaacc1b6bfa955fa10841b593ab9b07
Alex Cristea <alex@thinslices.com>
2016-04-25 19:30:14 +0300

    Add Error Handling, Type Casting and Nested Types chapters

---
+++ swift-book-practice-sheet.playground/Pages/20. Nested Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+//:
+//: ## Nested Types
+//:
+//: Enumerations are often created to support a specific class or structure’s functionality.
+//:
+//: It can be convenient to define utility classes and structures purely for use within the context of a more complex type.
+//:
+//: To nest a type within another type, write its definition within the outer braces of the type it supports.
+//:
+//:----
+//: ### Nested Types in Action
+//:
+//: The BlackJack structure contains two nested enumeration types called Suit and Rank
+
+//:----
+//: ### Referring to Nested Types
+//:
+//: To use a nested type outside of its definition context, prefix its name with the name of the type it is nested within
+
+//: [Next](@next)
